319510f9752bab1b67331b9d0c3e19d8ef04ce14
Luis Gerhorst <luis@luisgerhorst.de>
2015-07-26 17:29:14 +0200

    Test device code grant

--- test/authorization_SUITE.erl
+++ test/authorization_SUITE.erl
@@ -9,6 +9,8 @@ all() ->
     [client_credentials
      %% Disabled so testing does not require interaction. Worked 15-07-22:
      %% authorization_code
+     %% Worked 15-07-26:
+     %% device_code
     ].
 
 init_per_suite(Config) ->
@@ -35,11 +37,36 @@ receive_authorization_code(Url) ->
 
 client_credentials(_Config) ->
     {ok, Tokens} = rdio_api_authorization:tokens_with_client_credentials(),
-    {ok, _Result, _NewTokens} = 
+    {ok, _Result, _NewTokens} =
         rdio_api:request(
-          "search", 
+          "search",
           [{"query", "Chiddy Bang"},
-           {"types", "Artist"}], 
+           {"types", "Artist"}],
           Tokens).
-    
 
+device_code(Config) ->
+    ct:timetrap({minutes, 1}),
+    {ok, DeviceCode, VerificationUrl, ExpirationTimestamp, PollingInterval} = rdio_api_authorization:start_device_code_grant(),
+    ct:print(" Open~n~s~n in a browser and enter the code~n~s~n to allow the application to access your account. Testing will then continue automatically within a few seconds.~n", [VerificationUrl, DeviceCode]),
+    Tokens = device_code_grant_poll_token_endpoint(DeviceCode, ExpirationTimestamp, PollingInterval, Config),
+    ct:timetrap({minutes, 1}),
+    {ok, _Result, _NewTokens} = rdio_api:request("currentUser", [], Tokens).
+
+device_code_grant_poll_token_endpoint(DeviceCode, ExpirationTimestamp, PollingInterval, Config) ->
+    ct:timetrap({seconds, PollingInterval + 60}),
+    Now = now_seconds(),
+    if ExpirationTimestamp < Now ->
+            device_code(Config);
+       true ->
+            case rdio_api_authorization:tokens_with_device_code(DeviceCode) of
+                {ok, Tokens} ->
+                    Tokens;
+                {error, {rdio, <<"pending_authorization">>, _Description}} ->
+                    timer:sleep(PollingInterval * 1000),
+                    device_code_grant_poll_token_endpoint(DeviceCode, ExpirationTimestamp, PollingInterval, Config)
+            end
+    end.
+
+now_seconds() ->
+    {Mega, Secs, _} = now(),
+    Mega*1000000 + Secs.
